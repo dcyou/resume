4990b3c9a4c18d962d1304ba90e80857f3dd51e1
dcyou <david@dcyou.fr>
2019-09-06 16:38:27 +0200

    fix the image position in the avatar tag

--- components/About.vue
+++ components/About.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h3>{{about.about_title ? about.about_title : "About Me"}}</h3>
         <el-row v-if="about.about_profile_image">
-            <el-col :span="8"><p><el-avatar :size="100" :fit="fill" :src="about.about_profile_image"></el-avatar></p></el-col>
+            <el-col :span="8"><p><el-avatar shape="circle" :size="100" fit="fill" :src="about.about_profile_image"></el-avatar></p></el-col>
             <el-col :span="16"><p v-html="about.about_content"></p></el-col>
         </el-row>   
         <el-row v-else>
