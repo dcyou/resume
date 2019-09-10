883ac1fecba5a41b693e667cecbf49c1b0d01d61
dcyou <david@dcyou.fr>
2019-09-10 10:47:35 +0200

    use description provide by package.json as page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_description || "",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
