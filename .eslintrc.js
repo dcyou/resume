da520dbf4f76357036e05f3b2978c49364cbb891
dcyou <david@dcyou.fr>
2019-10-07 16:34:50 +0200

    docs: implement eslint-plugin-jsdoc package in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,13 @@ module.exports = {
       // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
       // préférer utiliser `plugin:vue/strongly-recommended` ou `plugin:vue/recommended` pour des règles stictes.
       "plugin:vue/recommended",
-      "plugin:prettier/recommended"
+      "plugin:prettier/recommended",
+      "plugin:jsdoc/recommended"
     ],
     // required to lint *.vue files
     plugins: [
-      'vue'
+      'vue',
+      'jsdoc'
     ],
     // add your custom rules here
     rules: {
